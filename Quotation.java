bd37939c251c75ed3c4b87bfd1ea8a4d294b7a28
Waxyach <max20080106@gmail.com>
2026-02-03 14:16:30 +0200

    Delete Quotation.java

--- Quotation.java
+++
@@ -1,9 +0,0 @@
-public class Quotation {
-
-    String author = "Thomas Edison";
-    String quote = "Your worth consists in what you are and not in what you have.";
-
-    public void display() {
-        System.out.printf("%s (%s)%n", quote, author);
-    }
-}
