6df6fc2af1c9c41861dc809a20f8f4c2075a5c46
Waxyach <max20080106@gmail.com>
2026-02-03 14:14:19 +0200

    task complete

---
+++ src/Quotation.java
@@ -0,0 +1,9 @@
+public class Quotation {
+
+    String author = "Thomas Edison";
+    String quote = "Your worth consists in what you are and not in what you have.";
+
+    public void display() {
+        System.out.printf("%s (%s)%n", quote, author);
+    }
+}
